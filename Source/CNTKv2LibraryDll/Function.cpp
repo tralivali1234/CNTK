f30082571a241b4663858baec0c83de4e9c79037
Philipp Kranen <pkranen@microsoft.com>
2016-10-19 01:39:56 +0200

    minor fix

--- Source/CNTKv2LibraryDll/Function.cpp
+++ Source/CNTKv2LibraryDll/Function.cpp
@@ -1370,7 +1370,7 @@ namespace CNTK
         case PrimitiveOpType::ROIPooling:
         {
             auto roiOutputShape = functionConfig[PrimitiveFunction::AttributeNameROIOutputShape].Value<NDShape>();
-            computationNodePtr = New<ROIPoolingNode<ElementType>>(network->GetDeviceId(), functionName, AsTensorShape(roiOutputShape));
+            computationNodePtr = New<ROIPoolingNode<ElementType>>(network->GetDeviceId(), internalNodeName, AsTensorShape(roiOutputShape));
             break;
         }
         case PrimitiveOpType::Pooling:
